527e252bdc0661d95b36153b4371ed9be6a554f8
Renee <renee.xu321@gmail.com>
2024-08-07 18:02:02 -0400

    ann starter code

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@ void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
 	if (pressed) {
-		// pros::lcd::set_text(2, "I was pressed!");
+		pros::lcd::set_text(2, "I was pressed!");
 	} else {
-		// pros::lcd::clear_line(2);
+		pros::lcd::clear_line(2);
 	}
 }
 
@@ -23,10 +23,10 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	/* pros::lcd::initialize();
+	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button); */
+	pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -73,23 +73,22 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	
+	pros::Motor left_1(1); 
+	pros::Motor left_2(4);
+	pros::Motor right_1(2);
+	pros::Motor right_2(3);
 
+	double y = master.get_analog(ANALOG_LEFT_Y);
+	double x = master.get_analog(ANALOG_RIGHT_X);
 
-	while (true) {
-		/* pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+	double left = y + x;
+	double right = y - x;
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update */
-	}
+	left_1.move(left);
+	left_2.move(left);
+	right_1.move(right);
+	right_2.move(right);
 }
